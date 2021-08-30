1c332b6183b786032000f4c65d84e731681ebbea
Kostik <bobra_tebe@mail.ru>
2021-08-30 18:37:13 +0300

    Init commit

---
+++ libopenvpn.cpp
@@ -0,0 +1,5 @@
+#include "libopenvpn.h"
+
+LibOpenvpn::LibOpenvpn()
+{
+}
---
+++ libopenvpn.h
@@ -0,0 +1,13 @@
+#ifndef LIBOPENVPN_H
+#define LIBOPENVPN_H
+
+#include "libOpenvpn_global.h"
+#include "ovpncli.hpp"
+
+class LIBOPENVPN_EXPORT LibOpenvpn
+{
+public:
+    LibOpenvpn();
+};
+
+#endif // LIBOPENVPN_H

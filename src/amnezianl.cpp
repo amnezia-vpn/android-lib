55cc8055d303e9fca2978adc4fb47f73d1194525
Kostik <bobra_tebe@mail.ru>
2021-09-13 14:08:33 +0300

    Interface creating

--- src/amnezianl.cpp
+++ src/amnezianl.cpp
@@ -2,4 +2,33 @@
 
 Amnezianl::Amnezianl()
 {
+    client = new OpenVPNClientAndroid;
+}
+
+bool OpenVPNClientAndroid::pause_on_connection_timeout() {
+    return false;
+}
+
+void OpenVPNClientAndroid::event(const openvpn::ClientAPI::Event &event) {
+    bool error = event.error;
+    std::string name = event.name;
+    std::string info = event.info;
+    std::cout << "EVENT: err=" << error << "\n";
+    std::cout << "EVENT: name=" << name << "\n";
+    std::cout << "EVENT: info=" << info << "\n";
+}
+
+void OpenVPNClientAndroid::log(const openvpn::ClientAPI::LogInfo& loginfo) {
+    std::string text = loginfo.text;
+    std::cout << "LOG: " << text;
+}
+
+void OpenVPNClientAndroid::external_pki_cert_request(openvpn::ClientAPI::ExternalPKICertRequest& req) {
+    req.error = true;
+    req.errorText = "cert request failed: external PKI not implemented";
+}
+
+void OpenVPNClientAndroid::external_pki_sign_request(openvpn::ClientAPI::ExternalPKISignRequest& req) {
+    req.error = true;
+    req.errorText = "sign request failed: external PKI not implemented";
 }
--- src/amnezianl.h
+++ src/amnezianl.h
@@ -2,12 +2,30 @@
 #define AMNEZIANL_H
 
 #include "amnezianl_global.h"
-#include "client/ovpncli.hpp"
+//#include "client/ovpncli.hpp"
+#include "client/ovpncli.cpp"
+//#include "openvpn/client/clievent.hpp"
+
+class OpenVPNClientAndroid;
 
 class AMNEZIANL_EXPORT Amnezianl
 {
 public:
-    Amnezianl();
+    AMNEZIANL_EXPORT Amnezianl();
+    AMNEZIANL_EXPORT OpenVPNClientAndroid* getInterfacePointer() const {
+        return client;
+    }
+
+private:
+    OpenVPNClientAndroid* client;
+};
+
+class OpenVPNClientAndroid: public openvpn::ClientAPI::OpenVPNClient {
+    virtual bool pause_on_connection_timeout();
+    virtual void event(const openvpn::ClientAPI::Event &event);
+    virtual void log(const openvpn::ClientAPI::LogInfo& loginfo);
+    virtual void external_pki_cert_request(openvpn::ClientAPI::ExternalPKICertRequest& req);
+    virtual void external_pki_sign_request(openvpn::ClientAPI::ExternalPKISignRequest& req);
 };
 
 #endif // AMNEZIANL_H

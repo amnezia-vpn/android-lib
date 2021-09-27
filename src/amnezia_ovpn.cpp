2f722bd42a566446c399a4d18ff277dd98fb7945
Розов Никита Валерьевич <nikita.rozov@osinit.com>
2021-09-27 18:38:41 +0300

    Add temp functions for connection

--- src/amnezia_ovpn.cpp
+++ src/amnezia_ovpn.cpp
@@ -1,34 +1,75 @@
+#include <iostream>
+#include <fstream>
 #include "amnezia_ovpn.h"
 #include "client/ovpncli.hpp"
 
 using namespace openvpn::ClientAPI;
 
+#include <openvpn/time/timestr.hpp>
+
+using namespace openvpn;
+
 namespace AmneziaVPN {
     class AmneziaOpenVPN : public OpenVPNClient {
+    public:
+        bool setConfig(const std::string& aConfigFileName)
+        {
+            std::ifstream lConfigFile;
+
+            lConfigFile.open(aConfigFileName);
+
+            if (lConfigFile.is_open() == false) {
+                return false;
+            }
+
+            std::string lConfigData{};
+
+            std::stringstream lFileStream;
+            lFileStream << lConfigFile.rdbuf();
+            lConfigData = lFileStream.str();
+
+            Config lConfig;
+
+            lConfig.content = lConfigData;
+
+            ProvideCreds lCreds;
+            //username from config or GUI
+            lCreds.username = "user"; //temp
+             //password from config or GUI
+            lCreds.password = "pass"; //temp
+
+            provide_creds(lCreds);
+
+            EvalConfig lEval = eval_config(lConfig);
+
+            return lEval.error == false;
+        }
 
         void event(const Event & event) override
         {
-
+            if (event.name == "CONNECTED") {
+                std::cout << "VPN connected succesfully" << std::endl;
+            }
         }
 
         void log(const LogInfo & log) override
         {
-
+             std::cout << date_time() << ' ' << log.text << std::flush;
         }
 
         bool pause_on_connection_timeout() override
         {
-
+            return false;
         }
 
         void external_pki_cert_request(ExternalPKICertRequest&) override
         {
-
+               //not impl
         }
 
         void external_pki_sign_request(ExternalPKISignRequest&) override
         {
-
+              //not impl
         }
 
     };
@@ -58,7 +99,9 @@ static AmneziaOpenVPN * ovpn_instance;
     ErrorCode OpenVPNProtocol::start()
     {
         // Configure connection before connect
+        ovpn_instance->setConfig("path/to/config.ovpn");
         ovpn_instance->connect();
+        return ErrorCode::OK;
     }
 
     void OpenVPNProtocol::stop()
@@ -71,5 +114,19 @@ static AmneziaOpenVPN * ovpn_instance;
 
     }
 
+    std::string OpenVPNProtocol::returnStatus()
+    {
+        // TEMP
+        ovpn_instance->setConfig("path/to/config.ovpn");
+        Status status = ovpn_instance->connect();
+
+        if (status.error)
+        {
+            return "Status: "+ status.status + " Messge: " + status.message;
+        }
+
+        return "Success";
+
+    }
 };
 
--- src/amnezia_ovpn.h
+++ src/amnezia_ovpn.h
@@ -1,10 +1,11 @@
 #pragma once
 
 #include "amneziainterface.h"
-
+#include <string>
 namespace AmneziaVPN {
 
 class OpenVPNProtocol : public VPNProtocol {
+public:
     OpenVPNProtocol();
 
     bool isConnected() const override;
@@ -12,6 +13,7 @@ class OpenVPNProtocol : public VPNProtocol {
     ErrorCode start() override;
     void stop() override;
     ConnectionState connectionState() const override;
+    std::string returnStatus();
 };
 
 }
